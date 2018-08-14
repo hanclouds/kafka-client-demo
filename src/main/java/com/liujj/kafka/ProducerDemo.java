31862b497622353fca21a71a50ade77ad5d6fa89
liujj <liujunjie@longshine.com>
2018-08-14 19:20:37 +0800

    commit

---
+++ src/main/java/com/liujj/kafka/ProducerDemo.java
@@ -0,0 +1,62 @@
+package com.liujj.kafka;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import javax.security.auth.login.Configuration;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @program: kafkaclientdemo
+ * @description:
+ * @author: liujj
+ * @create: 2018-08-14 14:21
+ **/
+public class ProducerDemo {
+    /**
+     * 使用producer 注释掉consumer部分的代码及 group.id，key.deserializer，value.deserializer 属性
+     * @param args
+     */
+    public static void main(String[] args){
+        Properties props = new Properties();
+
+        java.security.Security.setProperty("login.configuration.provider", "com.liujj.kafka.SaslConfig");
+        props.put("bootstrap.servers", "192.168.1.100:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("group.id", "test-consumer-group");
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT");
+        props.put("sasl.mechanism", "PLAIN");
+        Configuration.setConfiguration(new SaslConfig());
+        // 使用producer 注释掉consumer部分的代码及 group.id 属性
+//        Producer<String, String> producer = new KafkaProducer<>(props);
+//        for (int i = 0; i < 100; i++){
+//            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+//        }
+//        producer.close();
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records){
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+
+        }
+
+
+    }
+}
---
+++ src/main/java/com/liujj/kafka/SaslConfig.java
@@ -0,0 +1,29 @@
+package com.liujj.kafka;
+
+import javax.security.auth.login.AppConfigurationEntry;
+import javax.security.auth.login.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: kafkaclientdemo
+ * @description:
+ * @author: liujj
+ * @create: 2018-08-14 18:01
+ **/
+public class SaslConfig extends Configuration {
+    @Override
+    public AppConfigurationEntry[] getAppConfigurationEntry(String name) {
+        String appKey = "WL4odQnH";
+        String secretKey = "mCVmZAka-fS4EKMWTYKS1zZ84";
+        Map<String, String> options = new HashMap<String, String>();
+        options.put("username", appKey);
+        options.put("password", secretKey);
+        AppConfigurationEntry entry = new AppConfigurationEntry(
+                "org.apache.kafka.common.security.plain.PlainLoginModule",
+                AppConfigurationEntry.LoginModuleControlFlag.REQUIRED, options);
+        AppConfigurationEntry[] configurationEntries = new AppConfigurationEntry[1];
+        configurationEntries[0] = entry;
+        return configurationEntries;
+    }
+}
