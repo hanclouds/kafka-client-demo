42bdf325ec477e00272bb0a67e1ab7574fdd1388
liujj <liujunjie@longshine.com>
2019-01-04 15:38:50 +0800

    增加设备事件和设备命令的topic订阅

--- src/main/java/com/hanclouds/cloud/sample/ConsumerClientDemo.java
+++ src/main/java/com/hanclouds/cloud/sample/ConsumerClientDemo.java
@@ -12,7 +12,6 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
